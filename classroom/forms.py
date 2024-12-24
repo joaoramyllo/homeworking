e677bd62c04c06b413d4bee41ca1cf70bbfcee91
joaoramyllo <joaoramyllo.dev@gmail.com>
2024-12-24 16:45:12 -0300

    Novo Collectstatic

--- classroom/forms.py
+++ classroom/forms.py
@@ -4,15 +4,16 @@ from django.db import transaction
 from django.forms.utils import ValidationError
 from django.forms import DateInput
 
-from classroom.models import (Answer, Question, Student, StudentAnswer,
-                              Subject, User, List, Avatar)
+from classroom.models import Answer, Question, Student, StudentAnswer, Subject, User, List, Avatar
+
 
 class QuizSearchForm(forms.Form):
     search_materia_inexact = forms.CharField(
         required=False,
-        label='Buscar por matéria ou assunto',
-        widget=forms.TextInput(attrs={'placeholder': 'Buscar'}),
+        label="Buscar por matéria ou assunto",
+        widget=forms.TextInput(attrs={"placeholder": "Buscar"}),
     )
+
 
 class TeacherSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -27,15 +28,17 @@ class TeacherSignUpForm(UserCreationForm):
 
 
 class StudentSignUpForm(UserCreationForm):
-    interests = forms.ModelMultipleChoiceField(
-        queryset=Subject.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=True
-    )
+    interests = forms.ModelMultipleChoiceField(queryset=Subject.objects.all(), widget=forms.CheckboxSelectMultiple, required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2',)
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+        )
 
     @transaction.atomic
     def save(self):
@@ -43,23 +46,21 @@ class StudentSignUpForm(UserCreationForm):
         user.is_student = True
         user.save()
         student = Student.objects.create(user=user)
-        student.interests.add(*self.cleaned_data.get('interests'))
+        student.interests.add(*self.cleaned_data.get("interests"))
         return user
 
 
 class StudentInterestsForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ('interests', )
-        widgets = {
-            'interests': forms.CheckboxSelectMultiple
-        }
+        fields = ("interests",)
+        widgets = {"interests": forms.CheckboxSelectMultiple}
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ('text',)
+        fields = ("text",)
 
 
 class BaseAnswerInlineFormSet(forms.BaseInlineFormSet):
@@ -68,29 +69,25 @@ class BaseAnswerInlineFormSet(forms.BaseInlineFormSet):
 
         has_one_correct_answer = False
         for form in self.forms:
-            if not form.cleaned_data.get('DELETE', False):
-                if form.cleaned_data.get('is_correct', False):
+            if not form.cleaned_data.get("DELETE", False):
+                if form.cleaned_data.get("is_correct", False):
                     has_one_correct_answer = True
                     break
         if not has_one_correct_answer:
-            raise ValidationError('Selecione pelo menos uma resposta como correta.', code='no_correct_answer')
+            raise ValidationError("Selecione pelo menos uma resposta como correta.", code="no_correct_answer")
 
 
 class TakeQuizForm(forms.ModelForm):
-    answer = forms.ModelChoiceField(
-        queryset=Answer.objects.none(),
-        widget=forms.RadioSelect(),
-        required=True,
-        empty_label=None)
+    answer = forms.ModelChoiceField(queryset=Answer.objects.none(), widget=forms.RadioSelect(), required=True, empty_label=None)
 
     class Meta:
         model = StudentAnswer
-        fields = ('answer', )
+        fields = ("answer",)
 
     def __init__(self, *args, **kwargs):
-        question = kwargs.pop('question')
+        question = kwargs.pop("question")
         super().__init__(*args, **kwargs)
-        self.fields['answer'].queryset = question.answers.order_by('text')
+        self.fields["answer"].queryset = question.answers.order_by("text")
 
 
 class ListForm(forms.ModelForm):
@@ -98,41 +95,33 @@ class ListForm(forms.ModelForm):
         model = List
 
         widgets = {
-            'start_date': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
-            'end_date': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+            "start_date": DateInput(attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M"),
+            "end_date": DateInput(attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M"),
         }
-        exclude = ['list_student', 'completed']
+        exclude = ["list_student", "completed"]
 
 
 def __init__(self, *args, **kwargs):
     super(ListForm, self).__init__(*args, **kwargs)
     # input_formats to parse HTML5 datetime-local input to datetime field
-    self.fields['start_date'].input_formats = ('%Y-%m-%dT%H:%M',)
-    self.fields['end_date'].input_formats = ('%Y-%m-%dT%H:%M',)
-
+    self.fields["start_date"].input_formats = ("%Y-%m-%dT%H:%M",)
+    self.fields["end_date"].input_formats = ("%Y-%m-%dT%H:%M",)
 
 
 class PrimaryAvatarForm(forms.Form):
-    answer = forms.ModelChoiceField(
-        queryset=Avatar.objects.none(),
-        widget=forms.RadioSelect(),
-        required=True,
-        empty_label=None)
+    answer = forms.ModelChoiceField(queryset=Avatar.objects.none(), widget=forms.RadioSelect(), required=True, empty_label=None)
 
     class Meta:
         model = Avatar
-        fields = ('avatar',)
+        fields = ("avatar",)
 
     """
     def __init__(self, *args, **kwargs):
         question = kwargs.pop('avatar_user')
         super().__init__(*args, **kwargs)
         choices = [(avatar.id, avatar_img(avatar, size)) for avatar in avatars]
-        self.fields['choice'] = forms.ChoiceField(label=_("Choices"),
-                                                  choices=choices,
-                                                  widget=widgets.RadioSelect)
+        self.fields['choice'] = forms.ChoiceField(label=_("Choices"),choices=choices,widget=widgets.RadioSelect)
     """
 
 
-#Importado completamente
-
+# Importado completamente
